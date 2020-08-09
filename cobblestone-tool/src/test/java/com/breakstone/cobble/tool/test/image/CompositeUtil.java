e7773e922791040cc04bd21feaadc36fef975f50
siqigang <siqigang@meituan.com>
2020-08-09 23:54:02 +0800

    util

--- cobblestone-tool/src/test/java/com/breakstone/cobble/tool/test/image/CompositeUtil.java
+++ cobblestone-tool/src/test/java/com/breakstone/cobble/tool/test/image/CompositeUtil.java
@@ -5,6 +5,7 @@ import org.im4java.core.CompositeCmd;
 import org.im4java.core.ConvertCmd;
 import org.im4java.core.GMOperation;
 import org.im4java.core.IMOperation;
+import org.im4java.utils.BatchConverter;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -52,7 +53,7 @@ public class CompositeUtil {
         // with,height
 //        op.geometry(200,500);
         // width, height,x,y
-        op.geometry(500,500,-200,300);
+        op.geometry(500, 500, -200, 300);
         op.addImage(imagePath("2.png"));
 
         op.addImage(imagePath("bg.png"));
@@ -61,6 +62,76 @@ public class CompositeUtil {
         exeCompositeCmd(op, true);
     }
 
+    @Test
+    public void composite4() {
+        IMOperation op = new IMOperation();
+        // with,height
+//        op.geometry(200,500);
+        // width, height,x,y
+        op.geometry(500, 500, -200, 300);
+        op.addImage(imagePath("2.png"));
+
+        // 添加背景
+        op.size(800, 1200);
+        op.addRawArgs("xc:gray50");
+        op.addImage(imagePath("r4.png"));
+
+
+        exeCompositeCmd(op, true);
+    }
+
+    enum GravityEnum {
+        NorthWest,
+        North,
+        NorthEast,
+        West,
+        Center,
+        East,
+        SouthWest,
+        South,
+        SouthEast
+    }
+
+    @Test
+    public void composite5() {
+        IMOperation op = new IMOperation();
+        // with,height
+//        op.geometry(200,500);
+        // width, height,x,y
+        // 方位
+        op.gravity(GravityEnum.East.name());
+        // 大小，起始位置
+        op.geometry(500, 500, -200, 300);
+        op.addImage(imagePath("2.png"));
+
+        // 添加背景
+        op.size(800, 1200);
+        op.addRawArgs("xc:gray50");
+        op.addImage(imagePath("r5.png"));
+
+
+        exeCompositeCmd(op, true);
+    }
+
+    @Test
+    public void composite6() {
+        IMOperation op = new IMOperation();
+        // 方位
+        op.gravity(GravityEnum.East.name());
+        // 大小，起始位置
+        op.geometry(500, 500, -200, 300);
+
+        op.addImage(imagePath("2.png"));
+
+
+        // 添加背景
+        op.size(800, 1200);
+        op.addRawArgs("xc:gray50");
+        op.addImage(imagePath("r6.png"));
+
+        exeCompositeCmd(op, true);
+
+    }
 
 
     private static boolean exeConvertCmd(IMOperation operation, boolean useGraphicsMagick) {
