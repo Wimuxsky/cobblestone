84320c23588d1115726b95132dce980ad78ace9b
siqigang <siqigang@meituan.com>
2020-09-22 16:57:18 +0800

    res

---
+++ cobblestone-core/src/test/java/com/breakstone/cobble/core/test/combine/CombineBean.java
@@ -0,0 +1,24 @@
+package com.breakstone.cobble.core.test.combine;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CombineBean {
+
+    private int index;
+
+    private double scoreSum;
+
+    /**
+     * 组合中各个元素的候选素材Index
+     */
+    private int[] candidateIndexArr;
+
+
+}
--- cobblestone-core/src/test/java/com/breakstone/cobble/core/test/combine/CombineTest.java
+++ cobblestone-core/src/test/java/com/breakstone/cobble/core/test/combine/CombineTest.java
@@ -12,87 +12,148 @@ import java.util.List;
 public class CombineTest {
 
     @Test
-    public void test() {
+    public void testA() {
         // 2图1文
-        List<CombineElement> pic2List = getCandidateList(0, 2, 30, 1);
-        List<CombineElement> txt1List = getCandidateList(2, 1, 30, 2);
+        pic2_txt1_1();
+
+        // 1图2文
+//        pic1_txt2_1();
+    }
+
+    @Test
+    public void testB() {
+        // 2图1文
+        pic2_txt1_2();
+
+        // 1图2文
+//        pic1_txt2_2();
+    }
+
+    /**
+     * 2图1文-方案1
+     */
+    public void pic2_txt1_1() {
+        List<CombineBean> pic1List = getCandidateList(0, 3, 30, 1.0d);
+        List<CombineBean> pic2List = getCandidateList(1, 3, 30, 1.0d);
+        List<CombineBean> txt1List = getCandidateList(2, 3, 30, 2.0d);
+
+        List<CombineBean> aList = combineCombine(pic1List, pic2List, 3);
+        List<CombineBean> combineList = combineCombine(aList, txt1List, 3);
+        Collections.shuffle(combineList);
+        sort(combineList);
+        combineList = combineList.subList(0, 100);
+        print(combineList);
+    }
+
+    /**
+     * 2图1文-方案2
+     */
+    public void pic2_txt1_2() {
+        List<CombineBean> pic1List = getCandidateList(0, 3, 30, 1.0d);
+        List<CombineBean> pic2List = getCandidateList(1, 3, 30, 1.0d);
+        List<CombineBean> txt1List = getCandidateList(2, 3, 30, 2.0d);
+
+        List<CombineBean> aList = combineCombine(pic1List, pic2List, 3);
+        Collections.shuffle(aList);
+        sort(aList);
+        reset(aList, 1.0d);
+//        List<CombineBean> picList = aList.subList(0, 30);
 
 
-        List<CombineElement> aList = combineCandidate(pic2List.get(0), pic2List.get(1), 3);
-        List<CombineElement> bList = combineCandidate(txt1List.get(0), null, 3);
-
-        List<CombineElement> combineList = combineCombineList(aList, bList);
-        Collections.sort(combineList, Comparator.comparingInt(x -> x.getElementIndexSum()));
-        for (int i = 0; i < combineList.size(); i++) {
-            combineList.get(i).setIndex(i);
-        }
+        List<CombineBean> combineList = combineCombine(aList, txt1List, 3);
+        Collections.shuffle(combineList);
+        sort(combineList);
+        combineList = combineList.subList(0, 100);
+        combineList.forEach(combineBean -> combineBean.setScoreSum(combineBean.getCandidateIndexArr()[0] + combineBean.getCandidateIndexArr()[1] + combineBean.getCandidateIndexArr()[2] * 2));
         print(combineList);
     }
 
 
-    private void print(List<CombineElement> combineList) {
-        combineList.forEach(x -> System.out.println(x.getIndex() + ":\t" + x.getElementIndexSum() + "\t:\t" + JSON.toJSONString(x.getElementIndexArr())));
+    public void pic1_txt2_1() {
+        List<CombineBean> pic1List = getCandidateList(0, 3, 30, 1.0d);
+        List<CombineBean> txt1List = getCandidateList(1, 3, 30, 2.0d);
+        List<CombineBean> txt2List = getCandidateList(2, 3, 30, 2.0d);
+
+        List<CombineBean> aList = combineCombine(txt1List, txt2List, 3);
+        List<CombineBean> combineList = combineCombine(pic1List, aList, 3);
+        Collections.shuffle(combineList);
+        sort(combineList);
+        combineList = combineList.subList(0, 100);
+        print(combineList);
     }
 
-    private List<CombineElement> getCandidateList(int startIndex, int elementCount, int candidateSize, int factor) {
-        List<CombineElement> candidateList = Lists.newArrayList();
-        for (int i = 0; i < elementCount; i++) {
-            candidateList.add(CombineElement.generateCandidate(startIndex + i, candidateSize, factor));
+    public void pic1_txt2_2() {
+        List<CombineBean> pic1List = getCandidateList(0, 3, 30, 1.0d);
+        List<CombineBean> txt1List = getCandidateList(1, 3, 30, 2.0d);
+        List<CombineBean> txt2List = getCandidateList(2, 3, 30, 2.0d);
+
+        List<CombineBean> aList = combineCombine(txt1List, txt2List, 3);
+        Collections.shuffle(aList);
+        sort(aList);
+        reset(aList, 2.0d);
+//        List<CombineBean> txtList = aList.subList(0, 30);
+
+        List<CombineBean> combineList = combineCombine(pic1List, aList, 3);
+        Collections.shuffle(combineList);
+        sort(combineList);
+        combineList = combineList.subList(0, 100);
+        combineList.forEach(combineBean -> combineBean.setScoreSum(combineBean.getCandidateIndexArr()[0] + combineBean.getCandidateIndexArr()[1] * 2 + combineBean.getCandidateIndexArr()[2] * 2));
+        print(combineList);
+    }
+
+    private void sort(List<CombineBean> combineList) {
+        Collections.sort(combineList, Comparator.comparingDouble(x -> x.getScoreSum()));
+        for (int i = 0; i < combineList.size(); i++) {
+            combineList.get(i).setIndex(i);
+        }
+    }
+
+    private void reset(List<CombineBean> aList, double factor) {
+        for (int i = 0; i < aList.size(); i++) {
+            aList.get(i).setScoreSum((i + 1) * factor);
+        }
+    }
+
+
+    private void print(List<CombineBean> combineList) {
+
+        combineList.forEach(x -> System.out.println(x.getIndex() + ":\t" + x.getScoreSum() + "\t:\t" + JSON.toJSONString(x.getCandidateIndexArr())));
+//        combineList.forEach(x -> System.out.println(x.getIndex() + ":\t" + JSON.toJSONString(x.getCandidateIndexArr())));
+
+    }
+
+
+    private List<CombineBean> getCandidateList(int elementIndex, int elementCount, int candidateSize, double factor) {
+        List<CombineBean> candidateList = Lists.newArrayList();
+        for (int i = 0; i < candidateSize; i++) {
+            CombineBean combineBean = CombineBean.builder()
+                    .scoreSum((i + 1) * factor)
+                    .candidateIndexArr(new int[elementCount])
+                    .build();
+            combineBean.getCandidateIndexArr()[elementIndex] = i + 1;
+            candidateList.add(combineBean);
         }
         return candidateList;
     }
 
-
-    public List<CombineElement> combineCandidate(CombineElement candidateA, CombineElement candidateB, int elementCount) {
-        List<CombineElement> combineElementList = Lists.newArrayList();
-        if (null != candidateA && null != candidateB) {
-            for (int i = 0; i < candidateA.getElementIndexArr().length; i++) {
-                for (int j = 0; j < candidateB.getElementIndexArr().length; j++) {
-                    CombineElement combineElement = CombineElement.getInitCombineElement(elementCount);
-                    combineElement.getElementIndexArr()[candidateA.getIndex()] = candidateA.getElementIndexArr()[i];
-                    combineElement.getElementIndexArr()[candidateB.getIndex()] = candidateB.getElementIndexArr()[j];
-                    combineElement.setElementIndexSum(candidateA.getElementIndexArr()[i] + candidateB.getElementIndexArr()[j]);
-                    combineElementList.add(combineElement);
+    private List<CombineBean> combineCombine(List<CombineBean> aList, List<CombineBean> bList, int elementCount) {
+        if (CollectionUtils.isEmpty(aList) && CollectionUtils.isEmpty(bList)) {
+            throw new IllegalArgumentException("CombineCombine 参数异常");
+        }
+        List<CombineBean> combineList = Lists.newArrayList();
+        for (CombineBean combineA : aList) {
+            for (CombineBean combineB : bList) {
+                CombineBean combineBean = CombineBean.builder()
+                        .scoreSum(combineA.getScoreSum() + combineB.getScoreSum())
+                        .candidateIndexArr(new int[elementCount])
+                        .build();
+                for (int i = 0; i < elementCount; i++) {
+                    combineBean.getCandidateIndexArr()[i] = combineA.getCandidateIndexArr()[i] + combineB.getCandidateIndexArr()[i];
                 }
-            }
-        } else if (null != candidateA) {
-            for (int i = 0; i < candidateA.getElementIndexArr().length; i++) {
-                CombineElement combineElement = CombineElement.getInitCombineElement(elementCount);
-                combineElement.getElementIndexArr()[candidateA.getIndex()] = candidateA.getElementIndexArr()[i];
-                combineElement.setElementIndexSum(candidateA.getElementIndexArr()[i]);
-                combineElementList.add(combineElement);
+                combineList.add(combineBean);
             }
         }
 
-        return combineElementList;
+        return combineList;
     }
-
-
-    /**
-     * 组合 组合列表
-     *
-     * @param aList
-     * @param bList
-     * @return
-     */
-    public List<CombineElement> combineCombineList(List<CombineElement> aList, List<CombineElement> bList) {
-        if (CollectionUtils.isEmpty(bList)) {
-            return aList;
-        }
-        List<CombineElement> combineElementList = Lists.newArrayList();
-        for (CombineElement combineA : aList) {
-            for (CombineElement combineB : bList) {
-                CombineElement combineElement = CombineElement.getInitCombineElement(combineA.getElementIndexArr().length);
-                for (int i = 0; i < combineElement.getElementIndexArr().length; i++) {
-                    combineElement.getElementIndexArr()[i] = combineA.getElementIndexArr()[i] + combineB.getElementIndexArr()[i];
-                    CombineElement.calculateIndexSum(combineElement);
-                    combineElement.setElementIndexSum(combineA.getElementIndexSum() + combineB.getElementIndexSum());
-                }
-                combineElementList.add(combineElement);
-            }
-        }
-        return combineElementList;
-    }
-
-
 }
